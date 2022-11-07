7ceefe502a3061bba698ca576c164cf1a85ecfaa
Tahsin Can Koçum <tckocum@gmail.com>
2022-11-07 18:04:51 +0300

    Update Path.py

--- Path.py
+++ Path.py
@@ -6,7 +6,7 @@ Created on Mon Nov  7 16:21:53 2022
 """
 
 import numpy as np
-import
+import mathplotlib.pyplot as plt
 
 x1 = 20;
 y1 = 30;
@@ -25,6 +25,17 @@ A = np.array([ [1,x1,x1**2,x1**3],
 
 b = np.array([  [y1],
                 [y2],
-                [tan(q1)],
-                [tan(q2)]   
+                [np.tan(q1)],
+                [np.tan(q2)]   
             ])
+
+a_coef = np.linalg.inv(A) @ b
+
+a0 = a_coef[0];
+a1 = a_coef[1];
+a2 = a_coef[2];
+a3 = a_coef[3];
+
+X = np.linspace(x1,x2,100,endpoint=True)
+Y = a3*X**3 + a2*X**2 + a1*X + a0
+plt.plot(X,Y)
